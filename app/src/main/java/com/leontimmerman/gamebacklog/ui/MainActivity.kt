9d592b2429380a42e104a3da09b217f971f37438
Leon Timmerman <leontimmerman123@gmail.com>
2020-01-04 14:04:08 +0100

    added delete functions

--- app/src/main/java/com/leontimmerman/gamebacklog/ui/MainActivity.kt
+++ app/src/main/java/com/leontimmerman/gamebacklog/ui/MainActivity.kt
@@ -3,15 +3,22 @@ package com.leontimmerman.gamebacklog.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.leontimmerman.gamebacklog.R
 import com.leontimmerman.gamebacklog.model.Game
 import com.leontimmerman.gamebacklog.ui.viewmodel.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +35,27 @@ class MainActivity : AppCompatActivity() {
         initViewModel()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_delete_shopping_list -> {
+                CoroutineScope(Dispatchers.Main).launch {
+                    withContext(Dispatchers.IO) {
+                        mainActivityViewModel.deleteAllGames()
+                    }
+                }
+                gameAdapter.notifyDataSetChanged()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun initViews() {
         fabAdd.setOnClickListener{
             startActivity(Intent(this, AddActivity::class.java))
@@ -37,6 +65,9 @@ class MainActivity : AppCompatActivity() {
         rvGames.adapter = gameAdapter
 
         rvGames.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        // Add swipe delete
+        createItemTouchHelper().attachToRecyclerView(rvGames)
     }
 
     private fun initViewModel() {
@@ -47,5 +78,27 @@ class MainActivity : AppCompatActivity() {
             this@MainActivity.games.addAll(gamesFromDB)
             gameAdapter.notifyDataSetChanged()
         })
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        var callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                var gameToDelete = games[position]
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    withContext(Dispatchers.IO) {
+                        mainActivityViewModel.deleteGame(gameToDelete)
+                    }
+                }
+                gameAdapter.notifyDataSetChanged()
+            }
+        }
+
+        return ItemTouchHelper(callback)
     }
 }
